fd919b808fb4568837488f57b18a297aca5a7e3c
thiago.sundfeld <thiago.sundfeld@hotmail.com>
2018-10-09 10:31:23 -0300

    Feito o backend e integrado o front end. Está dando erro mas funciona.

--- js/itemlist.js
+++ js/itemlist.js
@@ -1,20 +1,44 @@
-let lista = new Array();
-let item;
-let nome;
-let preco;
-let qtd;
+let lista = [];
+let item = {};
+
+let valorfinal=0;
 
 function newitem() {
 
-    nome = prompt("Digite o item:","Desert Eagle");
-    preco = prompt("Digite o valor do item:","1000.00R$");
-    qtd = prompt("Digite a quantidade em estoque:","100");
-    new item(nome,preco,qtd);
+    let nome;
+    let preco;
+    let qtd;
+    nome = prompt("Digite o item:","Batata");
+    preco = prompt("Digite o valor do item:","1.00R$");
+    qtd = prompt("Digite a quantidade que deseja comprar:","100");
+    item.nome = nome;
+    item.preco = preco;
+    item.qtd = qtd;
     lista.push(item);
+    valorfinal = valorfinal + (item.preco*item.qtd);
+    document.getElementById("valor-total").innerHTML = valorfinal.toString();
+    refresh(item);
+
+}
+function refresh(obj) {
+    lista.forEach(showlist(obj));
+
 }
 
-function showlist() {
+function showlist(obj) {
 
-    let tempp = document.createElement("p");
-    tempp = document.createTextNode();
+    let temppnome = document.createElement("p");
+    let temptextnome = document.createTextNode(obj.nome);
+    temppnome.appendChild(temptextnome);
+    document.getElementById("lista-nome").appendChild(temppnome);
+
+    let tempppreco = document.createElement("p");
+    let temptextpreco = document.createTextNode(obj.preco.toString());
+    tempppreco.appendChild(temptextpreco);
+    document.getElementById("lista-preco").appendChild(tempppreco);
+
+    let temppqtd = document.createElement("p");
+    let temptextqtd = document.createTextNode(obj.qtd.toString());
+    temppqtd.appendChild(temptextqtd);
+    document.getElementById("lista-qtd").appendChild(temppqtd);
 }
